10ae385709e4b17ec39ef03447c5b6259df78e68
Kyle Berry <kyleryanberry@gmail.com>
2016-12-18 20:55:04 -0500

    Add game files

---
+++ 2d/Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody2D rbody;
+	Animator anim;
+	[SerializeField] int speed = 1;
+	
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector2 movement_v = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+		rbody.MovePosition(rbody.position+speed*movement_v*Time.deltaTime);
+	
+	}
+}
